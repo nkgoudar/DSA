9ec7c4d5b15cb7ff4443da238e14a5dba5adaf47
Naveenkumar <nkgoudar357@gmail.com>
2022-05-24 11:31:33 +0530

    Create Queues.js

---
+++ Queues.js
@@ -0,0 +1,47 @@
+/*
+
+FIRST IN FIRST OUT
+
+*/
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(value) {
+        const newNode = new Node(value);
+        if(this.size === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue() {
+        if(this.size === 0) return undefined;
+        const temp = this.first;
+        if(this.size === 1) {
+            this.first = null;
+            this.last = null;
+        } else {
+            this.first = this.first.next;
+        }
+        this.size -= 1;
+        return temp.value;
+    }
+}
+
+const q = new Queue();
